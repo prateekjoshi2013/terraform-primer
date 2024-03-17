ea369a8c4b17e0c9999998b58bf15d2c7fb7cc17
prateekjohi2013 <prateekjoshi2013@gmail.com>
2024-03-17 15:32:14 -0700

    added ami_instance resource terraform plan - to check the change sfrom last state terraform apply - to add the resource to aws terraform destroy - to delete all created resources

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ provider "aws"{
     profile="default"
     region="us-east-1"
 }
+
+resource "aws_instance" "app_server" {
+    ami = "ami-09748abeb7370d1bc"
+    instance_type = "t2.micro"
+    tags={
+        Name="my-tf"
+    }
+}
